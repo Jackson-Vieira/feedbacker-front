36524b1eb6c22209bd68f08b6df32ec9a8010f5a
Jackson-Vieira <jacksonvieiira12@gmail.com>
2023-03-14 15:11:00 -0400

    feat: create header logged

--- dashboard/src/views/Credentials/index.vue
+++ dashboard/src/views/Credentials/index.vue
@@ -1,7 +1,20 @@
 <template>
-  <h1 className="text-3xl font-bold underline text-brand-main">Credentials</h1>
+  <div class="flex justify-center w-full h-28 bg-brand-main">
+    <HeaderLogged />
+  </div>
+  <div class="flex flex-col items-center justify-center h-64 bg-brand-gray">
+    <h1 class="text-4xl font-black text-center text-gray-800">Credenciais</h1>
+    <p class="text-lg text-center text-gray-800 font-regular">Aqui você pode gerenciar suas credenciais</p>
+  </div>
 </template>
 
-<script></script>
+<script>
+import HeaderLogged from '@/components/HeaderLogged'
+export default {
+  components: {
+    HeaderLogged
+  }
+  }
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="postcss" scoped></style>
